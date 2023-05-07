e0773c7b4ad995b73f6cadfe75d6a4cde7f52d15
Benosman Hind <62807209+BenosmanHind@users.noreply.github.com>
2023-05-07 21:19:46 +0200

    Update front.blade.php

--- resources/views/layouts/front.blade.php
+++ resources/views/layouts/front.blade.php
@@ -127,7 +127,11 @@
                                     <li><a class="dropdown-toggle" role="button" id="dropdownMenuLink" data-toggle="dropdown"  aria-expanded="false"><i class="fal fa-user-circle "></i></a>
                                         <div class="dropdown-menu" aria-labelledby="dropdownMenuLink">
                                         @auth
+                                        @if(Auth::user()->type == 'admin')
+                                        <a class="dropdown-item" href="{{ asset('/admin') }}" style="color :#000">Dashboard</a>
+                                        @else
                                         <a class="dropdown-item" href="{{ asset('/app') }}" style="color :#000">Dashboard</a>
+                                        @endif
                                         <a href="{{route('logout')}}" class="dropdown-item ai-icon" onclick="event.preventDefault();
                                         document.getElementById('logout-form').submit();">
                                            <svg id="icon-logout" xmlns="http://www.w3.org/2000/svg" class="text-danger" width="18" height="18" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><path d="M9 21H5a2 2 0 0 1-2-2V5a2 2 0 0 1 2-2h4"></path><polyline points="16 17 21 12 16 7"></polyline><line x1="21" y1="12" x2="9" y2="12"></line></svg>
